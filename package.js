3edc8b13e5a6bb7a7f4a451e2f752d311f3b6140
Andre Wyrwa <andre.wyrwa@theiconic.com.au>
2017-02-05 15:59:49 +1100

    update minified file

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'gsklee:ngstorage',
-  version: '0.3.12',
+  version: '0.3.13',
   summary: 'ngStorage package for Meteor',
   git: 'https://github.com/gsklee/ngStorage',
   documentation: 'README.md'
